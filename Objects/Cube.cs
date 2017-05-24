67f50ab34ab570ab5241f462890c4b718272dea5
Andrew <expandrew@gmail.com>
2017-05-24 10:08:51 -0700

    incorporate functionality for finding the volume and surface area of a cube using the length of the user provided square.

--- Objects/Cube.cs
+++ Objects/Cube.cs
@@ -2,6 +2,15 @@ namespace RectangleChecker
 {
   public class Cube
   {
+    public int GetVolume()
+    {
+      int length = _face.GetLength();
+      return length * length * length;
+    }
+    public int GetSurfaceArea()
+    {
+      return _face.GetArea() * 6;
+    }
     private Rectangle _face;
     public Cube (Rectangle side)
     {
